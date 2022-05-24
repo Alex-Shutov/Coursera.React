2b5925e60baba78cfa1d85635a84c2fed198f906
Alex-Shutov <girasimov1337@gmail.com>
2022-05-25 01:21:11 +0500

    Final commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+    $('#loginModal').click(function (){
+        $('#logModal').modal('show');
+    });
+    $('#reserveButton').click(()=>{
+        $('#reserveModal').modal('show')
+    })
+});
